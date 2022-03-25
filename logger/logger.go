3d6012705fc1f30290388013efa2a87d786a4a21
mitry <mitry@mitrys-MacBook-Pro.local>
2022-03-25 18:56:28 -0400

    init commit

---
+++ logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+
+func init() {
+	var err error
+
+	config := zap.NewProductionConfig()
+
+	config.EncoderConfig = zap.NewProductionEncoderConfig()
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.StacktraceKey = ""
+
+	logger, err = config.Build(zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Info(message string, fields ...zap.Field) {
+	logger.Info(message, fields...)
+}
+
+func Debug(message string, fields ...zap.Field) {
+	logger.Debug(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	logger.Error(message, fields...)
+}
+
+func Fatal(message string, fields ...zap.Field) {
+	logger.Fatal(message, fields...)
+}
